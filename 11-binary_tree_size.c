4b848d417c61f8c6cea9806a0cac709b9ad08e8e
Oke Edafe Great <greatedafeoke@gmail.com>
2021-11-30 09:48:45 -0800

    Completed task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size -  measures the size of a binary tree
+ *
+ * @tree: pointer to root node
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) +
+			binary_tree_size(tree->right));
+}
