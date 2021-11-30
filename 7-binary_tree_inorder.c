8584636e32675dcf4e357e977632cdf736f936f0
Oke Edafe Great <greatedafeoke@gmail.com>
2021-11-30 09:09:27 -0800

    Completed task 9

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * binary_tree_inorder - goes through a binary tree using pre-order traversal
  *
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to call for each node
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree:  pointer to the root node of the tree to measure the height
+ *
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t c = 0;
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree)
+	{
+		if (tree->right || tree->left)
+			c = c + 1;
+		if (tree->left)
+			l += binary_tree_height(tree->left);
+		if (tree->right)
+			r += binary_tree_height(tree->right);
+	}
+	if (l > r)
+		c += l;
+	else
+		c += r;
+	return (c);
+}
