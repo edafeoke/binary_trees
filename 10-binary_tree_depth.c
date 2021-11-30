a7daee9a84bcfab7a603dc6ead2bc6e38948f8f6
Oke Edafe Great <greatedafeoke@gmail.com>
2021-11-30 09:25:39 -0800

    Completed task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_dept - measures the dept of a binary tree
+ *
+ * @tree:  pointer to the root node of the tree to measure the height
+ *
+ * Return: height of tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->parent)
+		return (1 + binary_tree_depth(tree->parent));
+	else
+		return (0);
+}
